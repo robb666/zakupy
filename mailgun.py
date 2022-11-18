c5b8a36127ac149058f0dd6a1cc569da1a332313
robb666 <56496611+robb666@users.noreply.github.com>
2022-11-18 23:11:09 +0100

    Update mailgun.py

--- mailgun.py
+++ mailgun.py
@@ -24,7 +24,7 @@ msg['To'] = "robert.patryk.grzelak@gmail.com"
 
 s = smtplib.SMTP('smtp.mailgun.org', 587)
 
-s.login('postmaster@sandbox7869ed4c3baf41c28d93ed01199eebc0.mailgun.org',
-        'd420511ffa1c2a5f1661508f81d2a1c5-46ac6b00-92cbad3b')
+s.login(os.environ.get("EMAIL_HOST_USER"),
+        os.environ.get("EMAIL_HOST_PASSWORD"))
 s.sendmail(msg['From'], msg['To'], msg.as_string())
 s.quit()
